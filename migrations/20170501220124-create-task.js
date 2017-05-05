40573b7ee381591c8dbe443cd2d07eb16db7d61f
Sarah Riehl <sls.riehl@gmail.com>
2017-05-05 16:09:26 -0500

    task migration

--- migrations/20170501220124-create-task.js
+++ migrations/20170501220124-create-task.js
@@ -1,33 +1,58 @@
 'use strict';
+
 module.exports = {
-  up: function(queryInterface, Sequelize) {
-    return queryInterface.createTable('Tasks', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      done: {
-        type: Sequelize.BOOLEAN
-      },
-      description: {
-        type: Sequelize.TEXT
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
+  up: function (queryInterface, Sequelize) {
+		return queryInterface.createTable('Tasks', {
+			id: {
+				allowNull: false,
+				primaryKey: true,
+				type: Sequelize.UUID,
+				defaultValue: Sequelize.UUIDV4
+			},
+			name: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			description: {
+				type: Sequelize.TEXT
+			},
+			done: {
+				type: Sequelize.BOOLEAN
+			},
+			ContextId: {
+				type: Sequelize.UUID,
+				onDelete: 'CASCADE',
+				references: {
+					model: 'Contexts',
+					key: 'id'
+				}
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
   },
-  down: function(queryInterface, Sequelize) {
-    return queryInterface.dropTable('Tasks');
+
+  down: function (queryInterface, Sequelize) {
+		return queryInterface.dropTable('Tasks');
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
   }
 };
