6f5afe63208c12424c4539dac3b888198657c095
Sarah Riehl <sls.riehl@gmail.com>
2017-05-05 14:18:56 -0500

    context model

--- models/context.js
+++ models/context.js
@@ -1,10 +1,22 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var Context = sequelize.define('Context', {
+		id: {
+			allowNull: false,
+			primaryKey: true,
+			type: DataTypes.UUID,
+			defaultValue: DataTypes.UUIDV4
+		},
     name: DataTypes.STRING
   }, {
     classMethods: {
       associate: function(models) {
+				Context.hasMany(models.Item)
+        Context.belongsTo(models.User, {
+					foreignKey: 'UserId',
+					onDelete: 'CASCADE'
+				});
+				//Context.hasOne(models.User, {foreignKey: 'UserId', onDelete: 'CASCADE'});
         // associations can be defined here
       }
     }
