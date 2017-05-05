093d4b94a6edd9ccf7ec5276e153fae95a202d08
Sarah Riehl <sls.riehl@gmail.com>
2017-05-05 14:37:23 -0500

    context migration

--- migrations/20170501220026-create-context.js
+++ migrations/20170501220026-create-context.js
@@ -1,27 +1,51 @@
 'use strict';
+
 module.exports = {
-  up: function(queryInterface, Sequelize) {
-    return queryInterface.createTable('Contexts', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
+  up: function (queryInterface, Sequelize) {
+		return queryInterface.createTable('Contexts', {
+			id: {
+				allowNull: false,
+				primaryKey: true,
+				type: Sequelize.UUID,
+				defaultValue: Sequelize.UUIDV4
+			},
+			name: {
+				type: Sequelize.STRING
+			},
+			UserId: {
+				type: Sequelize.UUID,
+				onDelete: 'CASCADE',
+				references: {
+					model: 'Users',
+					key: 'id'
+				}
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
   },
-  down: function(queryInterface, Sequelize) {
-    return queryInterface.dropTable('Contexts');
+
+  down: function (queryInterface, Sequelize) {
+		return queryInterface.dropTable('Contexts');
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
   }
 };
