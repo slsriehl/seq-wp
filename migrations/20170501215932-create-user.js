7eb436b107432ef3bf70c38402d966b932845a8f
Sarah Riehl <sls.riehl@gmail.com>
2017-05-05 14:18:39 -0500

    user migration

--- migrations/20170501215932-create-user.js
+++ migrations/20170501215932-create-user.js
@@ -1,30 +1,49 @@
 'use strict';
+
 module.exports = {
-  up: function(queryInterface, Sequelize) {
-    return queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      password: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
+  up: function (queryInterface, Sequelize) {
+		return queryInterface.createTable('Users', {
+			id: {
+				allowNull: false,
+				primaryKey: true,
+				type: Sequelize.UUID,
+				defaultValue: Sequelize.UUIDV4
+			},
+			email: {
+				type: Sequelize.STRING,
+				unique: true
+			},
+			password: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
   },
-  down: function(queryInterface, Sequelize) {
-    return queryInterface.dropTable('Users');
+
+  down: function (queryInterface, Sequelize) {
+		return queryInterface.dropTable('Users');
+
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
   }
 };
